013d95b79530b2b401cd8f25dbe26c91758b719c
MICKISZ ELOICK <eloick.mickisz@etu.unistra.fr>
2023-10-17 21:15:19 +0200

    add image for the technologie in the project (currently only laravel and react)

--- projets.php
+++ projets.php
@@ -6,12 +6,36 @@ class Projet
     private $id;
     private $derniereModification;
 
+    private $technologie;
+
     public function __construct($nom, $chemin, $id, $derniereModification)
     {
         $this->nom = $nom;
         $this->chemin = $chemin;
         $this->id = $id;
         $this->derniereModification = $derniereModification;
+        $this->technologie = $this->detecterTechnologie();
+    }
+
+    private function detecterTechnologie()
+    {
+        if (file_exists($this->chemin . '/composer.json')) {
+            $composer = json_decode(file_get_contents($this->chemin . '/composer.json'), true);
+            if (isset($composer['require']['laravel/framework'])) {
+                return 'laravel';
+            }
+        }
+
+        if (file_exists($this->chemin . '/package.json')) {
+            $package = json_decode(file_get_contents($this->chemin . '/package.json'), true);
+            if (isset($package['dependencies']['react'])) {
+                return 'react';
+            }
+        }
+
+        // Add more technology checks here...
+
+        return 'unknown';
     }
 
     public function afficher()
@@ -19,6 +43,7 @@ class Projet
         echo '<div class="card" id="' . $this->id . '" onclick="openWithVSCode(this)" data-path="' . $this->chemin . '">';
         echo '<h3>' . $this->nom . '</h3>';
         echo '<p>Dernière modification : ' . date("F d Y H:i:s.", $this->derniereModification) . '</p>';
+        echo '<img class="logo" src="images/' . $this->technologie . '.png" alt="' . $this->technologie . ' logo">';
         echo '</div>';
     }
 }
