362d8f390a8d1d0a94558c86e090f4621cc8ee4c
MICKISZ ELOICK <eloick.mickisz@etu.unistra.fr>
2023-10-18 09:51:46 +0200

    add ide selection

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var ide = "vscode-insiders"; // Set your IDE here
+
 function openWithVSCode(element) {
   const projectPath = element.getAttribute("data-path");
   const link = element.getAttribute("id");
@@ -6,10 +8,31 @@ function openWithVSCode(element) {
     const wslPath = projectPath.replace(/\\/g, "/");
     const link = "http://localhost:3000";
     window.open(link, "_blank");
-    window.open("vscode-insiders://file" + wslPath);
+    openInIDE(wslPath);
   } else {
     const localProjectPath = "D:/Sites/" + projectPath;
     window.open(link, "_blank");
-    window.open("vscode-insiders://file/" + localProjectPath);
+    openInIDE(localProjectPath);
+  }
+}
+
+function openInIDE(path) {
+  switch (ide) {
+    case "vscode":
+      window.open("vscode-insiders://file/" + path);
+      break;
+    case "vscode-insiders":
+      window.open("vscode-insiders://file/" + path);
+      break;
+    case "atom":
+      window.open("atom://core/open/file?filename=" + path);
+      break;
+    case "phpstorm":
+      window.open("phpstorm://open?file=" + path);
+      break;
+    // Add more cases for other IDEs as needed
+    default:
+      // Default code if no IDE matches
+      break;
   }
 }
